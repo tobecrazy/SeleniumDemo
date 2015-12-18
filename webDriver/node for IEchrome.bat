7b9ac2655dd64c1e961c63c0dfadc0326b4e9328
tobecrazy <dbyl@foxmail.com>
2015-12-18 22:17:04 +0800

    modified:   bin/com/dbyl/tests/MapTest.class 	modified:   build/classes/com/dbyl/libarary/utils/BasePage$1.class 	modified:   build/classes/com/dbyl/libarary/utils/BasePage$2.class 	modified:   build/classes/com/dbyl/libarary/utils/BasePage$3.class 	modified:   build/classes/com/dbyl/libarary/utils/BasePage.class 	modified:   test-output/Suite/testng-failed.xml 	modified:   test-output/html/overview.html 	modified:   test-output/html/suite1_test1_results.html 	modified:   test-output/testng-failed.xml 	modified:   webDriver/node for IEchrome.bat
    	.gradle/
    	build.gradle
    local.properties
    	webDriver/standaloneServer.bat

--- webDriver/node for IEchrome.bat
+++ webDriver/node for IEchrome.bat
@@ -1,1 +1,1 @@
-java -Dwebdriver.ie.driver="C:\Users\Young\workspace\Demo\webDriver\IEDriverServer.exe" -Dwebdriver.chrome.driver="C:\Users\Young\workspace\Demo\webDriver\chromedriver.exe" -jar selenium-server-standalone-2.48.0.jar -role node -hub http://127.0.0.1:4444/grid/register
+java -Dwebdriver.ie.driver="IEDriverServer.exe" -Dwebdriver.chrome.driver="chromedriver.exe" -jar selenium-server-standalone-2.48.0.jar -role node -hub http://127.0.0.1:4444/grid/register
