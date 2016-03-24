7464d4e194aedb10e022ea420cfd2f5c8d693d4f
tobecrazy <dbyl@foxmail.com>
2016-03-24 22:10:55 +0800

    add alipayLogin Demo

--- src/com/dbyl/tests/alipayLoginDemo.java
+++ src/com/dbyl/tests/alipayLoginDemo.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.os.WindowsUtils;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,30 +13,24 @@ import org.testng.annotations.Test;
 
 import com.dbyl.libarary.utils.DriverFactory;
 
-public class Hy {
+public class alipayLoginDemo {
 	private WebDriver driver;
 
 	@Test(groups = { "edgeDriver" })
 	public void edgeDriver() throws InterruptedException {
 		WindowsUtils.getProgramFilesPath();
-		driver =DriverFactory.getEDGEDriver();
-		driver.get("https://www.baidu.com");
+		driver =DriverFactory.getChromeDriver();
+		driver.get("https://www.alipay.com/i/indexNew.htm");
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		// 打开
-		WebElement inputBox =driver.findElement(By.id("kw"));
-		inputBox.sendKeys("edge browser");
-		By searchBy= By.id("su");
-		Thread.sleep(3000);
-		if (isElementPresent(driver, searchBy))
-		{
-			driver.findElement(searchBy).click();
-		}
-		System.out.println(driver.getTitle());
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.quit();
-		 
+		WebElement loginbutton=driver.findElement(By.xpath("//a[@class='am-button button-login']"));
+		loginbutton.click();
+		driver.switchTo().frame("J_loginIframe");
+		WebElement username = driver.findElement(By.id("J-input-user"));
+		username.sendKeys("test");
+		WebElement password=driver.findElement(By.id("password_rsainput"));
+		password.sendKeys("aaaaa");
 
 	}
 
