a71f06cacf9af064eee12b5e6875a5c8fecc5510
tobecrazy <dbyl@foxmail.com>
2016-03-31 00:35:21 +0800

    use log4j

--- src/com/dbyl/libarary/utils/PageFactory.java
+++ src/com/dbyl/libarary/utils/PageFactory.java
@@ -5,27 +5,34 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * 
+ * @author Young
+ *
+ */
 public class PageFactory {
-	public synchronized static Object getPage(Class<?> key,WebDriver d)
-			throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
-		String test = key.getCanonicalName();
-		System.out.println(test);
-		Class<?> clazz = Class.forName(test);
+	static Log log = new Log(PageFactory.class);
+
+	public synchronized static Object getPage(Class<?> key, WebDriver d)
+			throws ClassNotFoundException, NoSuchMethodException,
+			SecurityException, IllegalArgumentException,
+			InvocationTargetException {
+		String pageClassName = key.getCanonicalName();
+		log.info("page name is " + pageClassName);
+		Class<?> clazz = Class.forName(pageClassName);
 		Object obj = null;
 		try {
-		    Constructor<?> constructor = clazz.getConstructor(WebDriver.class);
+			Constructor<?> constructor = clazz.getConstructor(WebDriver.class);
 			obj = constructor.newInstance(d);
-			 
 
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
+			log.error(e.getMessage());
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
+			log.error(e.getMessage());
 			e.printStackTrace();
 		}
 		return obj;
-
 	}
 
 }
---
+++ src/com/dbyl/tests/getAlert.java
@@ -0,0 +1,83 @@
+package com.dbyl.tests;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.os.WindowsUtils;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import com.dbyl.libarary.utils.DriverFactory;
+
+public class getAlert {
+	private WebDriver driver;
+
+	@Test(groups = { "ChromeDriver" })
+	public void FireFoxDriver() throws InterruptedException {
+		WindowsUtils.getProgramFilesPath();
+		driver =DriverFactory.getFirefoxDriver();
+		driver.get("file:///G:/html/index.html");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		WebElement target1=driver.findElement(By.xpath("//a[@id='target1']"));
+		String currentWindow=driver.getWindowHandle();
+		String target1Window = null;
+		target1.click();
+		Set<String> windows = driver.getWindowHandles();
+		for(String window:windows)
+		{
+			 if(!currentWindow.equals(window))
+			 {
+				 target1Window=window;
+				 driver.switchTo().window(window);
+			 }
+		}
+		WebElement target2 = driver.findElement(By.id("target2"));
+		target2.click();
+		
+		 windows = driver.getWindowHandles();
+			for(String window:windows)
+			{
+				 if(!currentWindow.equals(window)&& !target1Window.equals(window) )
+				 {
+					 driver.switchTo().window(window);
+				 }
+			}
+	Alert  alert=driver.switchTo().alert();
+	System.out.println(alert.getText());
+	alert.accept();
+
+	}
+
+	/**
+	 * @author Young
+	 * @param driver
+	 * @param by
+	 * @return
+	 */
+	public static boolean isElementPresent(WebDriver driver, final By by) {
+
+		boolean isPresent = false;
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		isPresent = wait.until(new ExpectedCondition<WebElement>() {
+			@Override
+			public WebElement apply(WebDriver d) {
+				return d.findElement(by);
+			}
+		}).isDisplayed();
+		return isPresent;
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
