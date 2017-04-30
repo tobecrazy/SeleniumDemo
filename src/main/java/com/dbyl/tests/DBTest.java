95d294d86e0541bd858d10a6220848cf321fb0b8
tobecrazy <dbyl@foxmail.com>
2017-04-30 23:54:23 +0800

    clean

--- src/main/java/com/dbyl/tests/DBTest.java
+++ src/main/java/com/dbyl/tests/DBTest.java
@@ -4,18 +4,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.testng.annotations.Test;
+
 import com.mysql.jdbc.Connection;
 
 import main.java.com.dbyl.libarary.utils.DatabaseUtils;
 
 public class DBTest {
 
-	public DBTest() {
-	}
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Connection conn = (new DatabaseUtils.Builder().setHost("WWW.WP.COM").setDbName("study")
-				.setUser("study").setPassword("123456").builder()).getConection();
+	@Test(groups = { "bdbTest" })
+	public void dbTest() throws ClassNotFoundException, SQLException {
+		Connection conn = (new DatabaseUtils.Builder().setHost("WWW.WP.COM").setDbName("study").setUser("study")
+				.setPassword("123456").builder()).getConection();
 		Statement state = conn.createStatement();
 		ResultSet result = state.executeQuery("show databases");
 		while (result.next()) {
