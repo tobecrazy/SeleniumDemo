b60dcef01744c7af300410fa83aacc09f0659de5
tobecrazy <dbyl@foxmail.com>
2017-06-14 23:50:00 +0800

    demo study

---
+++ src/main/java/com/dbyl/tests/crawler/Crawler36Kr.java
@@ -0,0 +1,57 @@
+package main.java.com.dbyl.tests.crawler;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.monitor.SpiderMonitor;
+import us.codecraft.webmagic.pipeline.PageModelPipeline;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+
+import javax.management.JMException;
+import java.io.IOException;
+
+/**
+ * @author young
+ */
+@TargetUrl("http://www.36kr.com/p/\\d+.html")
+@HelpUrl("http://www.36kr.com/#/page/\\d+")
+public class Crawler36Kr {
+
+	@ExtractBy("//h1[@class='entry-title sep10']")
+	private String title;
+
+	@ExtractBy("//div[@class='mainContent sep-10']/tidyText()")
+	private String content;
+
+	@ExtractByUrl
+	private String url;
+
+	public static void main(String[] args) throws IOException, JMException {
+		// Just for benchmark
+		Spider thread = OOSpider.create(Site.me().setSleepTime(0), new PageModelPipeline() {
+			@Override
+			public void process(Object o, Task task) {
+
+			}
+		}, Crawler36Kr.class).thread(20).addUrl("http://www.36kr.com/");
+		thread.start();
+		SpiderMonitor spiderMonitor = SpiderMonitor.instance();
+		spiderMonitor.register(thread);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+}
--- src/main/java/com/dbyl/tests/crawler/JobCrawler.java
+++ src/main/java/com/dbyl/tests/crawler/JobCrawler.java
@@ -3,6 +3,7 @@ package main.java.com.dbyl.tests.crawler;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class JobCrawler implements PageProcessor {
@@ -11,16 +12,25 @@ public class JobCrawler implements PageProcessor {
 
 	@Override
 	public void process(Page page) {
-		page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
-		page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-		page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+		System.out.println("==============================================================");
+		System.out.println("                              Start                           ");
+		System.out.println("==============================================================");
+		page.addTargetRequests(page.getHtml().xpath("//table[@class='listC']//td/a").links()
+				.regex("(http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectInfo.aspx\\?code=\\d+.)").all());
+		page.putField("name", page.getHtml().xpath("//span[@id='PROJECT_XMMC1']/text()").toString());
 		if (page.getResultItems().get("name") == null) {
 			// skip this page
 			page.setSkip(true);
 		}
-		page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+		page.getRawText();
+		page.putField("nextPage", page.getHtml().xpath("//a[@id='PageNavigator1_LnkBtnNext']"));
+	
 		System.out.println(page.getResultItems());
-//		System.out.println(page.getHtml().toString());
+
+		System.out.println(page.getHtml().toString());
+		System.out.println("==============================================================");
+		System.out.println("                             The End                          ");
+		System.out.println("==============================================================");
 	}
 
 	@Override
@@ -29,7 +39,8 @@ public class JobCrawler implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		Spider.create(new JobCrawler()).addUrl("https://github.com/code4craft").thread(1).run();
+		Spider.create(new JobCrawler()).addUrl("http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectList.aspx")
+				.thread(1) .addPipeline(new JsonFilePipeline("/Volumes/Transcend/Document/workspace/Demo/logs")).run();
 	}
 
 }
