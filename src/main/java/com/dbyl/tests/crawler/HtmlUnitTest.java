dead198c4f503deb4b285e4f0141f6be6c0471e0
tobecrazy <dbyl@foxmail.com>
2017-06-18 00:37:52 +0800

    实现爬虫翻页

--- src/main/java/com/dbyl/tests/crawler/HtmlUnitTest.java
+++ src/main/java/com/dbyl/tests/crawler/HtmlUnitTest.java
@@ -2,27 +2,68 @@ package main.java.com.dbyl.tests.crawler;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.LinkedHashSet;
 import java.util.List;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import main.java.com.dbyl.libarary.utils.StringTools;
+
 public class HtmlUnitTest {
 
 	private static WebClient driver;
+	static String code;
 
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		driver = new WebClient();
+		driver = new WebClient(BrowserVersion.CHROME);
 		HtmlPage page = driver.getPage("http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectList.aspx");
+		LinkedHashSet<String> map = new LinkedHashSet<String>();
 
-        //get list of all divs
-        final List<?> links = page.getByXPath("//table[@class='listC']//td/a");
-        for(Object link:links)
-        {
-        	System.out.println(link.toString());
-        }
-         
+		DomElement button = page.getFirstByXPath("//a[@id='PageNavigator1_LnkBtnNext' and not(@disabled)]");
+		List<?> target = page.getByXPath("//table[@class='listC']//td/a");
+
+		for (Object link : target) {
+
+			code = getCode(link.toString());
+			map.add(code);
+		}
+		int i = 0;
+		while (null != page) {
+			i++;
+			System.out.println("============>" + i);
+			button = page.getFirstByXPath("//a[@id='PageNavigator1_LnkBtnNext' and not(@disabled)]");
+			if (null == button) {
+				break;
+			} else {
+				page = button.click();
+				target = null;
+				target = page.getByXPath("//table[@class='listC']//td/a");
+				for (Object link : target) {
+					code = getCode(link.toString());
+					map.add(code);
+				}
+			}
+		}
+
+		System.out.println(map.size());
 
 	}
 
+	/**
+	 * @author young
+	 * @param source
+	 * @return
+	 */
+	public static String getCode(String source) {
+		if (null != source) {
+			return StringTools.getMatch(source, "\\d+");
+		} else {
+			return null;
+		}
+
+	}
 }
--- src/main/java/com/dbyl/tests/crawler/JobCrawler.java
+++ src/main/java/com/dbyl/tests/crawler/JobCrawler.java
@@ -1,12 +1,15 @@
 package main.java.com.dbyl.tests.crawler;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.List;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-import main.java.com.dbyl.libarary.utils.DriverFactory;
+import main.java.com.dbyl.libarary.utils.StringTools;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -14,24 +17,26 @@ import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class JobCrawler implements PageProcessor {
-	static WebDriver driver = DriverFactory.getInstance().getChromeDriver();
 	private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+	private static WebClient driver;
+	static String code;
 
 	@Override
 	public void process(Page page) {
 		System.out.println("==============================================================");
 		System.out.println("                              Start                           ");
 		System.out.println("==============================================================");
-		page.addTargetRequests(page.getHtml().xpath("//table[@class='listC']//td/a").links()
-				.regex("(http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectInfo.aspx\\?code=\\d+.)").all());
 		page.putField("name", page.getHtml().xpath("//span[@id='PROJECT_XMMC1']/text()").toString());
-		if (page.getResultItems().get("name") == null) {
+		page.putField("price", page.getHtml().xpath("//span[@id='lbYsZZJj']/text()").toString());
+		if (null == page.getResultItems().get("name")) {
 			// skip this page
 			page.setSkip(true);
 		}
-		page.getRawText();
-		page.putField("nextPage", page.getHtml().xpath("//a[@id='PageNavigator1_LnkBtnNext']"));
 
+		if (null == page.getResultItems().get("price")) {
+			// skip this page
+			page.setSkip(true);
+		}
 		System.out.println(page.getResultItems());
 
 		System.out.println(page.getHtml().toString());
@@ -45,26 +50,68 @@ public class JobCrawler implements PageProcessor {
 		return site;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		driver.get("http://www.lhfdc.gov.cn/templets/lh/aspx/HPMS/ProjectList.aspx");
-		Spider.create(new JobCrawler()).addUrl("http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectList.aspx")
-				.thread(1).addPipeline(new JsonFilePipeline("/Volumes/Transcend/Document/workspace/Demo/logs")).run();
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		while (true) {
-			WebElement nextPage = driver
-					.findElement(By.xpath("//a[@id='PageNavigator1_LnkBtnNext' and not(@disabled)]"));
-			nextPage.click();
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-			String content = driver.getPageSource();
-			Page page = new Page();
-			page.setRawText(content);
-			Spider.create(new JobCrawler()).addUrl("http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectList.aspx")
-					.thread(1).addPipeline(new JsonFilePipeline("/Volumes/Transcend/Document/workspace/Demo/logs"))
-					.run();
+		for (String code : getAllLinks()) {
+			String url = "http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectInfo.aspx\\?code=" + code;
+			Spider.create(new JobCrawler()).addUrl(url).thread(5)
+					.addPipeline(new JsonFilePipeline("/Volumes/Transcend/Document/workspace/Demo/logs")).run();
 
 		}
+
 	}
 
+	/**
+	 * @author young
+	 * @return
+	 * @throws IOException
+	 */
+	public static LinkedHashSet<String> getAllLinks() throws IOException {
+		driver = new WebClient(BrowserVersion.CHROME);
+		HtmlPage page = driver.getPage("http://www.lhfdc.gov.cn/templets/lh/aspx/hpms/ProjectList.aspx");
+		LinkedHashSet<String> map = new LinkedHashSet<String>();
+
+		DomElement button = page.getFirstByXPath("//a[@id='PageNavigator1_LnkBtnNext' and not(@disabled)]");
+		List<?> target = page.getByXPath("//table[@class='listC']//td/a");
+
+		for (Object link : target) {
+			code = getCode(link.toString());
+			map.add(code);
+		}
+		int i = 0;
+		while (null != page) {
+			i++;
+			System.out.println("============>" + i);
+			button = page.getFirstByXPath("//a[@id='PageNavigator1_LnkBtnNext' and not(@disabled)]");
+			if (null == button) {
+				break;
+			} else {
+				page = button.click();
+				target = null;
+				target = page.getByXPath("//table[@class='listC']//td/a");
+				for (Object link : target) {
+					code = getCode(link.toString());
+					map.add(code);
+				}
+			}
+		}
+
+		System.out.println(map.size());
+		return map;
+
+	}
+
+	/**
+	 * @author young
+	 * @param source
+	 * @return
+	 */
+	public static String getCode(String source) {
+		if (null != source) {
+			return StringTools.getMatch(source, "\\d+");
+		} else {
+			return null;
+		}
+
+	}
 }
