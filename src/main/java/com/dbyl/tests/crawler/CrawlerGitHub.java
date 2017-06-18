6c8f902edb393bf1feba61de00abad5723183576
tobecrazy <dbyl@foxmail.com>
2017-06-18 20:00:10 +0800

    add demo

---
+++ src/main/java/com/dbyl/tests/crawler/CrawlerGitHub.java
@@ -0,0 +1,32 @@
+package main.java.com.dbyl.tests.crawler;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class CrawlerGitHub implements PageProcessor {
+	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+	public static void main(String[] args) {
+
+		Spider.create(new CrawlerGitHub()).addUrl("https://github.com/tobecrazy").thread(1).run();
+	}
+
+	@Override
+	public void process(Page page) {
+
+		page.putField("name", page.getHtml().xpath(("//span[@class='repo js-repo']/text()")).toString());
+		if (page.getResultItems().get("name") == null) {
+			// skip this page
+			page.setSkip(true);
+		}
+		System.out.println(page.getHtml());
+	}
+
+	@Override
+	public Site getSite() {
+		return site;
+	}
+
+}
