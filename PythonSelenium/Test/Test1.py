cdd47ce50648859a353c4edfabf86c495764c4a9
I321533 <young.liu@sap.com>
2017-06-22 11:43:40 +0800

    Test cases

--- PythonSelenium/Test/Test1.py
+++ PythonSelenium/Test/Test1.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-#-*-coding:utf-8-*-
+#coding=utf-8
 from selenium import webdriver
-driver=webdriver.Chrome(executable_path="c:/Users/Administrator/Documents/workspace/SeleniumDemo/webDriver/chromedriver.exe")
+import os,sys
+current_folder=os.getcwd()
+print(current_folder)
+driver=webdriver.Chrome(executable_path=current_folder+"/../../webDriver/chromedriver.exe")
 driver.get("http://www.baidu.com")
 inputbox=driver.find_element_by_id("kw")
 inputbox.send_keys("python")
 search_button=driver.find_element_by_id("su")
 search_button.click()
 title=driver.title
-print(title)
+#print(title.decode("utf8"))
 driver.quit()
 
 
