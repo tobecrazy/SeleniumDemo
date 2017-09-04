c8eace4332a6242ea98961bf8de0dd0748046e89
tobecrazy <dbyl@foxmail.com>
2017-09-05 00:03:23 +0800

    fix compile issue

--- src/main/java/com/dbyl/libarary/utils/CSVUtils.java
+++ src/main/java/com/dbyl/libarary/utils/CSVUtils.java
@@ -1,5 +1,12 @@
 package main.java.com.dbyl.libarary.utils;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 /**
  * 
  * @author young
@@ -19,8 +26,16 @@ public class CSVUtils {
 	 * @author young
 	 * @param path
 	 * @return
+	 * @throws IOException
 	 */
-	public boolean CSVWriter(String path) {
+	public boolean CSVWriter(String path) throws IOException {
+		Reader in = new FileReader("path/to/file.csv");
+		Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+		for (CSVRecord record : records) {
+			String lastName = record.get("Last Name");
+			String firstName = record.get("First Name");
+		}
+
 		return false;
 
 	}
