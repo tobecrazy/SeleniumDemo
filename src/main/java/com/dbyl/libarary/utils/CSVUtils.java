ccf9785a7eb4c93851261868675217d20b8fd073
tobecrazy <dbyl@foxmail.com>
2017-09-04 23:23:15 +0800

    add CSVUtils

---
+++ src/main/java/com/dbyl/libarary/utils/CSVUtils.java
@@ -0,0 +1,28 @@
+package main.java.com.dbyl.libarary.utils;
+
+/**
+ * 
+ * @author young
+ *
+ */
+public class CSVUtils {
+	/**
+	 * @author young
+	 * @param path
+	 * @return
+	 */
+	public static String[][] CSVReader(String path) {
+		return null;
+	}
+
+	/**
+	 * @author young
+	 * @param path
+	 * @return
+	 */
+	public boolean CSVWriter(String path) {
+		return false;
+
+	}
+
+}
