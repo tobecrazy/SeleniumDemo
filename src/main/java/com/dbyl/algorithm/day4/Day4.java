23b3cd3544c4bbf6d121dcfb3cad95647bab5898
tobecrazy <dbyl@foxmail.com>
2017-09-17 11:24:55 +0800

    upgrade geckodriver

--- src/main/java/com/dbyl/algorithm/day4/Day4.java
+++ src/main/java/com/dbyl/algorithm/day4/Day4.java
@@ -1,5 +1,7 @@
 package main.java.com.dbyl.algorithm.day4;
 
+import java.util.Random;
+
 import org.testng.annotations.Test;
 
 /**
@@ -14,14 +16,55 @@ public class Day4 {
 
 	@Test
 	public void test() {
-		romanToInt("45");
+		for (int i = 1; i <= 5000; i++) {
+			romanToInt(String.valueOf(5000));
+		}
+
 	}
 
 	public int romanToInt(String s) {
 
 		int temp = Integer.parseInt(s);
-		double a = Math.random();
-		System.out.println(a);
-		return 0;
+		Random r = new Random();
+		int result = r.nextInt(temp);
+		while (result > 3999 || result < 1) {
+			result = r.nextInt(temp);
+		}
+		System.out.println(result);
+		return result;
+	}
+
+	/**
+	 * @author young
+	 * @param moves
+	 * @return
+	 */
+	public boolean judgeCircle(String moves) {
+		int l = 0;
+		int r = 0;
+		int u = 0;
+		int d = 0;
+		char[] temps = moves.toCharArray();
+		for (char temp : temps) {
+			if (temp == 'U' || temp == 'u') {
+				u++;
+			}
+			if (temp == 'D' || temp == 'd') {
+				d++;
+			}
+			if (temp == 'L' || temp == 'l') {
+				l++;
+			}
+			if (temp == 'R' || temp == 'r') {
+				d++;
+			}
+		}
+
+		if ((l - r + u - d) == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
