9206f90917f538fe9fca9535f44bf17fc0ce6484
tobecrazy <dbyl@foxmail.com>
2017-10-15 13:21:02 +0800

    update pom

--- src/main/java/com/dbyl/tests/training/day4/WordPressTest.java
+++ src/main/java/com/dbyl/tests/training/day4/WordPressTest.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -17,6 +16,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
