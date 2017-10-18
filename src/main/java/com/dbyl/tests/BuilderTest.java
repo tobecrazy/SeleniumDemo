8fb9da8e1b7f853b3a80d1eefc816ef5b4d6cfb8
tobecrazy <dbyl@foxmail.com>
2017-10-18 22:24:44 +0800

    update testng xml

--- src/main/java/com/dbyl/tests/BuilderTest.java
+++ src/main/java/com/dbyl/tests/BuilderTest.java
@@ -1,11 +1,13 @@
 package main.java.com.dbyl.tests;
 
+import org.testng.annotations.Test;
+
 import main.java.com.dbyl.libarary.utils.DriverFlags;
 import main.java.com.dbyl.libarary.utils.Context.BrowserType;
 
 public class BuilderTest {
-
-	public static void main(String[] args) {
+	@Test(groups = { "BuilderTest" })
+	public void Test1() {
 		DriverFlags driver = new DriverFlags.Builder().setBrowserTyp(BrowserType.chrome).setHost("1111").build();
 		System.out.println(driver.getHost() + driver.getBrowserType());
 
