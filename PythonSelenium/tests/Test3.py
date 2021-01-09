d43acc683ea28862583c127cfb162a49f045300e
Young Liu <young.liu@sap.com>
2021-01-09 16:38:22 +0800

    remove old files

--- PythonSelenium/tests/Test3.py
+++ PythonSelenium/tests/Test3.py
@@ -1,32 +1,31 @@
-#coding:utf-8
+# coding:utf-8
 # script for python3
 import urllib3
 import ssl
 import unittest
-import os,sys
+import os, sys
 import time
 
+
 class Test:
-  '''
-  @author Young
-  @verson
-  Get page source
-  '''
-  def getPageSource(url):
-    http= urllib3.PoolManager()
-    try:
-      response = http.request('GET',url)
-      print ('loading ....')
-      print(response.status)
-      print(response.status)
-      return response.data
-    except:
-      print ('crul'+url)
+    '''
+    @author Young
+    @verson
+    Get page source
+    '''
 
+    def getPageSource(url):
+        http = urllib3.PoolManager()
+        try:
+            response = http.request('GET', url)
+            print('loading ....')
+            print(response.status)
+            print(response.status)
+            return response.data
+        except:
+            print('crul' + url)
 
-  if __name__ == "__main__":
-    print ('open www.baidu.com')
-    page=getPageSource("https://www.baidu.com")
-    print (page)
-    
-
+    if __name__ == "__main__":
+        print('open www.baidu.com')
+        page = getPageSource("https://www.baidu.com")
+        print(page)
---
+++ webDriver/README.md
@@ -0,0 +1,1 @@
+Since we have docker solution, I suggest you try docker , don't down load those drivers and standalone jars. Try docker solution!
